bddd63906222cc2f5672ecca4f8ee6e837fb4692
Abdmalik0 <abdprin@gmail.com>
2022-03-15 10:40:29 -0700

    y

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
-i
 int main(void)
 {
-	char -putchar[] = "_putchar";
+	char 0-putchar[] = "_putchar";
 
 	int i = 0;
 
-	while (-putchar[i] != '\0')
+	while (0-putchar[i] != '\0')
 	{
-		_putchar(-putchar[i]);
+		_putchar(0-putchar[i]);
 		i++;
 	}
 	_putchar('\n');
