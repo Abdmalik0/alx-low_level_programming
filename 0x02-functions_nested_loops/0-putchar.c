42d4b661a67e92dc3c2899f4f1a6c8b205377eff
Abdmalik0 <abdprin@gmail.com>
2022-03-15 10:14:21 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,6 @@
 i#include "main.h"
 
 
-
-
-/**
-
-
-* main - Print "_putchar" followed by a new line.
-
-
-* Description: You are not allowed to include standard libraries.
-
-
-* Return: 0
-
-
-*/
-
-
 int main(void)
 
 
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/**
+* main - Print "_putchar" followed by a new line.
+* Description: You are not allowed to include standard libraries.
+* Return: 0
+*/
+
+
